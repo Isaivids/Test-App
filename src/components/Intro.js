0671abf0f7e282b5a29735462df522fe537cef2c
IsaiVids <isaivids2020@gmail.com>
2022-04-05 22:05:41 +0530

    commit

---
+++ src/components/Intro.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import './Intro.css'
+
+const Intro = () => {
+  return (
+    <div className='intro'>
+        <div className='intro-left'>
+            Left
+        </div>
+        <div className='intro-right'>
+            Right
+        </div>
+    </div>
+  )
+}
+
+export default Intro
