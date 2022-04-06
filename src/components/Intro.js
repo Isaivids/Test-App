fa54eea45258ecbb94d7139695cc13132a8f3401
IsaiVids <isaivids2020@gmail.com>
2022-04-06 19:06:32 +0530

    commit

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -1,16 +1,30 @@
 import React from 'react'
 import './Intro.css'
+import Skills from './Skills'
 
 const Intro = () => {
   return (
+    <>
     <div className='intro'>
-        <div className='intro-left'>
-            Left
+      <div className='intro-left'>
+        <div className='intro-left-wrapper'>
+        <h2>Hii there</h2>
+        <h1>I'm Dilan Raj</h1>
+        <h3>Web Application Developer</h3>
+        <span>
+          Researching, designing, implementing, and managing software programs.
+          Testing and evaluating new programs.
+          Identifying areas for modification in existing programs and subsequently developing these modifications.
+          Writing and implementing efficient code.
+        </span>
         </div>
-        <div className='intro-right'>
-            Right
-        </div>
+      </div>
+      <div className='intro-right'>
+        Right
+      </div>
     </div>
+    <Skills className="skills"/>
+    </>
   )
 }
 
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,8 @@
-import { FaBeer } from 'react-icons/fa';
+import { BiMenuAltRight } from 'react-icons/bi';
+import { BsGithub } from 'react-icons/bs';
+import { FaWhatsapp } from 'react-icons/fa';
+import { SiNetlify } from 'react-icons/si';
+import {CgCloseO} from 'react-icons/cg'
 import React, { useState } from 'react'
 import { NavLink } from 'react-router-dom'
 import './Navbar.css'
@@ -10,16 +14,16 @@ const Navbar = () => {
         <div className='nav-wrapper'>
             <div className='nav-title'>
                 <h2>Dilan Raj</h2>
-                <h2 onClick={()=>setMenu(!menu)}>X</h2>
+                <h2>{menu ? <CgCloseO className='menu-icon' onClick={()=>setMenu(!menu)}/> : <BiMenuAltRight className='menu-icon' onClick={()=>setMenu(!menu)}/>}</h2>
             </div>
             <div className={menu ? 'nav-links' : "nav-links nav-active"} onClick={()=>setMenu(!menu)}>
                 <NavLink to="/" className="nav-font" >Intro</NavLink>
                 <NavLink to="/hobbies" className="nav-font" >Hobbies</NavLink>
             </div>
             <section className={menu ?'nav-icons':'nav-icons nav-active'} onClick={()=>setMenu(!menu)}>
-                <p><FaBeer/></p>
-                <p><FaBeer/></p>
-                <p><FaBeer/></p>
+                <p><BsGithub className='social-icon-1'/></p>
+                <p><FaWhatsapp className='social-icon-2'/></p>
+                <p><SiNetlify className='social-icon-3'/></p>
             </section>
             </div>
     </div>
