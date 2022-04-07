56e3fe2957cdcbc826a30ff560b4e17d598eafe2
IsaiVids <isaivids2020@gmail.com>
2022-04-07 18:58:12 +0530

    commit

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -2,6 +2,10 @@ import React from 'react'
 import './Intro.css'
 import Skills from './Skills';
 import { IoIosArrowDropdownCircle } from 'react-icons/io';
+import { FaReact } from 'react-icons/fa';
+import { FaHtml5 } from 'react-icons/fa';
+import { IoLogoJavascript } from 'react-icons/io';
+import { AiOutlineVideoCameraAdd } from 'react-icons/ai';
 import man from '../images/man.png'
 
 const Intro = () => {
@@ -13,13 +17,16 @@ const Intro = () => {
         <h2>Hii there</h2>
         <h1>I'm Dilan Raj</h1>
         <h3>Web Application Developer</h3>
+        <div className='intro-ico'>
+          <FaReact/><FaHtml5/><IoLogoJavascript/><AiOutlineVideoCameraAdd/>
+        </div>
         <span>
           Researching, designing, implementing, and managing software programs.
           Testing and evaluating new programs.
           Identifying areas for modification in existing programs and subsequently developing these modifications.
           Writing and implementing efficient code.
         </span>
-        <IoIosArrowDropdownCircle className='intro-icon'/>
+        <a href='#skills'><IoIosArrowDropdownCircle className='intro-icon'/></a>
         </div>
       </div>
       <div className='intro-right'>
--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,9 +1,56 @@
 import React from 'react'
 import './Skills.css'
+import { FaReact } from 'react-icons/fa';
+import { FaHtml5 } from 'react-icons/fa';
+import { SiAdobephotoshop } from 'react-icons/si';
+import { MdVideoSettings } from 'react-icons/md';
+import { AiFillDatabase } from 'react-icons/ai';
+
+
 function Skills() {
+
+const skills = [
+  {
+    tech : "React JS", Exp : "Intermediate", icon: <FaReact className='react'/>
+  },
+  {
+    tech : "HTML & CSS", Exp : "Advanced", icon: <FaHtml5 className='html'/>
+  },
+  {
+    tech : "Mainframe", Exp : "Beginner", icon: <AiFillDatabase className='mf'/>
+  },
+  {
+    tech : "Photoshop", Exp : "Expert", icon: <SiAdobephotoshop className='ps'/>
+  },
+  {
+    tech : "Video Editing", Exp : "Expert", icon: <MdVideoSettings className='vdo'/>
+  },
+]
+
   return (
-    <div className='skills'>Skills</div>
-  )
+  <div className='skills' id='skills'>
+    <div className='skills-left'>
+      <div className='container'>
+        {skills.map((skill)=>{
+          return (
+            <div className='content'>
+              <div>
+                <span>{skill.tech}</span>
+                <span>Expert Level : {skill.Exp}</span>
+              </div>
+              <div>
+              {skill.icon}
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+    <div className='skills-right'>
+        Right
+    </div> 
+  </div>
+  );
 }
 
-export default Skills
+export default Skills;
