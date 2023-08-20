634b2c7406ff9aa751bebb3276d85282c6d5bcf8
ISAIVIDS <isaivids2020@gmail.com>
2023-08-20 11:17:04 +0530

    intro page change

--- src/test/Intro.js
+++ src/test/Intro.js
@@ -1,31 +1,32 @@
-import React, { useRef } from 'react'
-import './intro.css'
-import Technical from './Technical'
-import { FaCss3 } from 'react-icons/fa'
-import { FaHtml5 } from 'react-icons/fa'
-import { FaReact } from 'react-icons/fa'
-import { SiAdobexd } from 'react-icons/si'
-import { FaJs } from 'react-icons/fa'
-import DemoProjects from './DemoProjects'
-import { NavLink } from 'react-router-dom'
+import React, { useRef } from "react";
+import "./intro.css";
+import Technical from "./Technical";
+import { FaCss3 } from "react-icons/fa";
+import { FaHtml5 } from "react-icons/fa";
+import { FaReact } from "react-icons/fa";
+import { SiAdobexd } from "react-icons/si";
+import { FaJs } from "react-icons/fa";
+import DemoProjects from "./DemoProjects";
+import { NavLink } from "react-router-dom";
 
 const Intro = () => {
-
   const skills = useRef(null);
   const demo = useRef(null);
 
-
-  const scrollPage =(ref) =>{
+  const scrollPage = (ref) => {
     window.scrollTo({
-      top: ref.offsetTop, 
+      top: ref.offsetTop,
       behavior: "smooth",
-    })
-  }
+    });
+  };
 
-const description =["Motivated junior web developer looking for a role as full-stack web developer. Passionate about building first-class web applications. Developed an e-commerce website for a local fashion."];
+  const description = [
+    "Motivated junior web developer looking for a role as full-stack web developer. Passionate about building first-class web applications. Developed an e-commerce website for a local fashion.",
+  ];
+  const desc1 = ["Hi I'm Dilan"];
   return (
     <>
-    <div className='intro'>
+      {/* <div className='intro'>
       <div className='intro-left'>
         <div className='wrapper-left'>
           <div className='content-left'>
@@ -56,9 +57,38 @@ const description =["Motivated junior web developer looking for a role as full-s
     <div className='pages'>
       <Technical skills={skills}/>
       <DemoProjects />
-    </div>
-    </>
-  )
-}
+    </div> */}
 
-export default Intro
+      <div className="home">
+        <div className="content">
+          <p>{desc1}</p>
+          <span>{description}</span>
+          <div className="buttons">
+            <NavLink
+              to="#skills"
+              className= "nav-link"
+              onClick={() => {
+                scrollPage(skills.current);
+              }}
+            >
+              Tech skills
+            </NavLink>
+            <NavLink
+              to="#demo"
+              className= "nav-link"
+              onClick={() => {
+                scrollPage(demo.current);
+              }}
+            >
+              Projects
+            </NavLink>
+          </div>
+        </div>
+      </div>
+      <Technical skills={skills} />
+      <DemoProjects />
+    </>
+  );
+};
+
+export default Intro;
--- src/test/Navbar.js
+++ src/test/Navbar.js
@@ -1,39 +1,80 @@
-import React, { useState } from 'react'
-import './Navbar.css'
-import { NavLink } from 'react-router-dom'
-import { FaCode } from 'react-icons/fa';
-import { BsGithub } from 'react-icons/bs';
-import { FaWhatsapp } from 'react-icons/fa';
-import { SiNetlify } from 'react-icons/si';
-
+import React, { useEffect, useState } from "react";
+import "./Navbar.css";
+import { NavLink } from "react-router-dom";
+import { FaCode } from "react-icons/fa";
+import { BsGithub } from "react-icons/bs";
+import { FaWhatsapp } from "react-icons/fa";
+import { SiNetlify } from "react-icons/si";
 
 const Navbar = () => {
+  const [toggle, setToggle] = useState(false);
+  const [background, setBackground] = useState(false);
 
-const [toggle, setToggle] = useState(false)
+  const handleScroll = () => {
+    const scrollPosition = window.scrollY;
+    scrollPosition >= 100 ? setBackground(true) : setBackground(false);
+  };
+  useEffect(() => {
+    handleScroll();
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
   return (
-    <div className='nav'>
-        <div className='nav-left'>
-            <div className='nav-name'>
-              <h3>Dilan Raj</h3>
-              <FaCode />
-              </div>
-            <div className={toggle?"nav-ham change":"nav-ham"} onClick={()=>setToggle(!toggle)}>
-              <div className="bar1"></div>
-              <div className="bar2"></div>
-              <div className="bar3"></div>
-            </div>
+    <div className={background ? "nav bg" : "nav"}>
+      <div className="nav-left">
+        <div className="nav-name">
+          <h3>Dilan Raj</h3>
+          <FaCode className="logo"/>
         </div>
-        <div className={toggle ?'nav-right show' : 'nav-right'}>
-            <NavLink className="navlinks" to="/" onClick={()=>setToggle(!toggle)}>Home</NavLink>
-            <NavLink className="navlinks" to="/contact" onClick={()=>setToggle(!toggle)}>Contact</NavLink>
-            <div className='nav-icons'>
-                <a href="https://github.com/Isaivids" rel="noreferrer" target="_blank"><BsGithub className='social-icon-1'/></a>
-                <a href="https://wa.me/7868047686" rel="noreferrer" target="_blank"><FaWhatsapp className='social-icon-2'/></a>
-                <a href="https://app.netlify.com/teams/isaivids/overview" rel="noreferrer" target="_blank"><SiNetlify className='social-icon-3'/></a>
-            </div>
+        <div
+          className={toggle ? "nav-ham change" : "nav-ham"}
+          onClick={() => setToggle(!toggle)}
+        >
+          <div className="bar1"></div>
+          <div className="bar2"></div>
+          <div className="bar3"></div>
         </div>
+      </div>
+      <div className={toggle ? "nav-right show" : "nav-right"}>
+        <NavLink
+          className={({ isActive }) => (isActive ? 'active' : 'navlinks')}
+          to="/"
+          onClick={() => setToggle(!toggle)}
+        >
+          Home
+        </NavLink>
+        <NavLink
+          className={({ isActive }) => (isActive ? 'active' : 'navlinks')}
+          to="/contact"
+          onClick={() => setToggle(!toggle)}
+        >
+          Contact
+        </NavLink>
+        <div className="nav-icons">
+          <a
+            href="https://github.com/Isaivids"
+            rel="noreferrer"
+            target="_blank"
+          >
+            <BsGithub className="social-icon-1" />
+          </a>
+          <a href="https://wa.me/7868047686" rel="noreferrer" target="_blank">
+            <FaWhatsapp className="social-icon-2" />
+          </a>
+          <a
+            href="https://app.netlify.com/teams/isaivids/overview"
+            rel="noreferrer"
+            target="_blank"
+          >
+            <SiNetlify className="social-icon-3" />
+          </a>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
